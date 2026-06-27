45886d672c3b9327e4368a3a4d70e14f32323139
Patrick Lowry <patio.lowry@gmail.com>
2026-06-27 16:52:27 -0700

    remove testing code from main

--- src/main.py
+++ src/main.py
@@ -19,36 +19,6 @@ def main() -> int:
     print(f"Edges: {graph.edge_count}")
     print(f"Vertices: {graph.vertex_count()}")
 
-    # # TODO: Remove this when done testing
-    # connections = [
-    #     { "movie": "The Odyssey", "actor": "Matt Damon" },
-    #     { "movie": "The Odyssey", "actor": "Tom Holland" },
-    #     { "movie": "The Odyssey", "actor": "Zendaya" },
-    #     { "movie": "The Odyssey", "actor": "Robert Pattinson" },
-    #     { "movie": "The Odyssey", "actor": "Anne Hathaway" },
-    #     { "movie": "The Devil Wears Prada", "actor": "Anne Hathaway" },
-    #     { "movie": "The Devil Wears Prada", "actor": "Meryl Streep" },
-    #     { "movie": "Mamma Mia", "actor": "Meryl Streep" },
-    #     { "movie": "Mamma Mia", "actor": "Amanda Seyfried" },
-    # ]
-
-    # graph = Graph()
-
-    # for connection in connections:
-    #     graph.add_connection(connection["actor"], connection["movie"])
-
-
-    # neighbors = graph.neighbors(1)
-    # print("Connections to The Odyssey:")
-    # for neighbor in neighbors:
-    #     vertex = graph.get_value(neighbor)
-    #     print(f"Name: {vertex.name}, Type: {vertex.type}")
-
-    # neighbors = graph.neighbors(5)
-    # print("Connections to Anne Hathaway:")
-    # for neighbor in neighbors:
-    #     vertex = graph.get_value(neighbor)
-    #     print(f"Name: {vertex.name}, Type: {vertex.type}")
     return 0
 
 if __name__ == "__main__":
