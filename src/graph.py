e31cdb16586c12ad36f97cb642297fc90129e393
Patrick Lowry <patio.lowry@gmail.com>
2026-06-27 16:50:41 -0700

    reorganize graph class

--- src/graph.py
+++ src/graph.py
@@ -26,6 +26,7 @@ class Graph:
     actor_indexes: dict[str, int]
     movie_indexes: dict[str, int]
 
+    #======  Constructor ======#
     def __init__(self) -> None:
         self.edge_count = 0
         self.vertices = []
@@ -33,38 +34,10 @@ class Graph:
         self.actor_indexes = {}
         self.movie_indexes = {}
     
-    def add_connection(self, actor: str, movie: str) -> None:
-        actor_idx = self._add_actor(actor)
-        movie_idx = self._add_movie(movie)
-        self._add_edge(actor_idx, movie_idx)
-    
-    def _add_actor(self, actor: str) -> int:
-        # Adds vertex for actor and returns index of the actor
-        idx = self.actor_indexes.get(actor.lower(), None)
-        if not idx:
-            idx = len(self.vertices)
-            new_vertex = Vertex(actor, "actor")
-            self.vertices.append(new_vertex)
-            self.edges.append(Edge(-1, None, None))
-            self.actor_indexes[actor.lower()] = idx
-        return idx
 
-    def _add_movie(self, movie: str) -> int:
-        # Adds vertex for movie and returns index of the movie
-        idx = self.movie_indexes.get(movie.lower(), None)
-        if not idx:
-            idx = len(self.vertices)
-            new_vertex = Vertex(movie, "movie")
-            self.vertices.append(new_vertex)
-            self.edges.append(Edge(-1, None, None))
-            self.movie_indexes[movie.lower()] = idx
-        return idx
-
+    #====== Accessors ======#
     def get_value(self, v: int) -> Vertex:
         return self.vertices[v]
-    
-    def set_value(self, v: int, val: Vertex) -> None:
-        self.vertices[v] = val
     
     def neighbors(self, v: int) -> List[int]:
         temp: List[int] = []
@@ -79,8 +52,19 @@ class Graph:
     
     def get_movie_index(self, name: str) -> Optional[int]:
         return self.movie_indexes.get(name.lower(), None)
-        
     
+    def get_actor_index(self, name: str) -> Optional[int]:
+        return self.actor_indexes.get(name.lower(), None)
+    
+
+    #====== Mutators ======#
+    def add_connection(self, actor: str, movie: str) -> None:
+        actor_idx = self._add_actor(actor)
+        movie_idx = self._add_movie(movie)
+        self._add_edge(actor_idx, movie_idx)   
+        
+        
+    #====== Internal helper functions ======#
     def _find(self, v: int, w: int) -> Edge | None:
         curr: Edge = self.edges[v]
         while (curr.next and curr.next.vertex < w):
@@ -102,4 +86,26 @@ class Graph:
             if curr.next.next:
                 curr.next.next.prev = curr.next
             self.edge_count += 1
+
+    def _add_actor(self, actor: str) -> int:
+        # Adds vertex for actor and returns index of the actor
+        idx = self.actor_indexes.get(actor.lower(), None)
+        if not idx:
+            idx = len(self.vertices)
+            new_vertex = Vertex(actor, "actor")
+            self.vertices.append(new_vertex)
+            self.edges.append(Edge(-1, None, None))
+            self.actor_indexes[actor.lower()] = idx
+        return idx
+
+    def _add_movie(self, movie: str) -> int:
+        # Adds vertex for movie and returns index of the movie
+        idx = self.movie_indexes.get(movie.lower(), None)
+        if not idx:
+            idx = len(self.vertices)
+            new_vertex = Vertex(movie, "movie")
+            self.vertices.append(new_vertex)
+            self.edges.append(Edge(-1, None, None))
+            self.movie_indexes[movie.lower()] = idx
+        return idx
         
